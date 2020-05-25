6f51994f4a964b483ede4f6ba4e3761b7248694b
Maksim Kuznetsov <makuznet@gmail.com>
2020-05-25 20:12:02 +0000

    Lesson 2

--- part2/3_2_task.rb
+++ part2/3_2_task.rb
@@ -6,7 +6,7 @@ array = [0, 1] # the first and the second Fibonacci numbers are fixed
 index = 2 # an array index
 element = 0 # Fibonacci number
 loop do
-  element = array[index - 1] + array[index - 2] # calculaing current Fibonacci number
+  element = array[index - 1] + array[index - 2] # calculating current Fibonacci number
   if element < 100 # separating calculation from assignment
     array[index] = element
     index += 1
