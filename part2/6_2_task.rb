1512eac48b28ac3bc4a6538e953ca2f4931a66f4
Maksim Kuznetsov <makuznet@gmail.com>
2020-05-25 19:57:36 +0000

    Lesson 2

--- part2/6_2_task.rb
+++ part2/6_2_task.rb
@@ -3,10 +3,10 @@
 
 list = {} # the main hash
 grand_total = 0
+puts 'Start entering items, their prices and q-ties to know how much you\'ve spent.'
+puts 'Type stop on item prompt to get results'
 loop do
   price_n_qty = {} # temp hash empted each cycle for nesting in the 'list' hash
-  puts 'Start entering items, their prices and q-ties to know how much you\'ve spent.'
-  puts 'Type stop on item prompt to get results'
   print 'item: '
   user_item = gets.chomp
   break if user_item == 'stop'
@@ -22,7 +22,7 @@ end
 puts list # printing out the main hash
 list.each do |item, nested_hash|
   cost = nested_hash[:price] * nested_hash[:qty] # calculating cost per item in the loop
-  puts "#{item} cost, EUR: #{cost}" # visualizing items costs kindly
+  puts "#{item} cost, EUR: #{cost.round(2)}" # visualizing items costs kindly
   grand_total += cost # incrementing grand total in the loop
 end
-print "Grand total, EUR: #{grand_total} \n" # displaying the final ammount spent
+print "Grand total, EUR: #{grand_total.round(2)} \n" # displaying the final ammount spent
