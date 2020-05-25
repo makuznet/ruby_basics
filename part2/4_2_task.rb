736adeba5112d3768a7788212a573b94ed764abc
Maksim Kuznetsov <makuznet@gmail.com>
2020-05-25 11:37:48 +0000

    Lesson 2

--- part2/4_2_task.rb
+++ part2/4_2_task.rb
@@ -2,23 +2,10 @@
 # frozen_string_literal: true
 
 # This script populates the resultant hash with vowels
-
 vowels_hash = {} # creating an empty hash to store the result
 vowels = %w[a e i o u] # creating an array with vowels as the style guide narrates
-
-=begin
-creating a hash comprising alphabet of two ranges converted to arrays
-.to_a -- to array
-a.zip(b) -- creates an array of arrays each comprising pairs a, b
-.to_h -- to hash
-=end
-alphabet_hash = ('a'..'z').to_a.zip(1..26).to_a.to_h
+alphabet_hash = ('a'..'z').to_a.zip(1..26).to_a.to_h # creating a hash comprising alphabet
 vowels.each do |vowel| # starting a loop to compare all vowels against the alphabet
-
-=begin
-.merge! -- populates itself with hash pairs
-.select -- starts hash searching mechanism
-=end
-vowels_hash.merge!(alphabet_hash.select { |letter, _index| letter == vowel })
+  vowels_hash.merge!(alphabet_hash.select { |letter, _index| letter == vowel })
 end
 puts vowels_hash
