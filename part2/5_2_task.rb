5904b46297e9ae963f453d792df2d3bed9ed5783
Maksim Kuznetsov <makuznet@gmail.com>
2020-05-25 14:31:52 +0000

    Lesson 2

--- part2/5_2_task.rb
+++ part2/5_2_task.rb
@@ -3,8 +3,8 @@
 
 puts 'Put in a date, ex. 22.2.1978, and get a day number in this year:'
 input = gets.chomp
-user_date = input.split('.').map!(&:to_i) # { |a| a.to_i }
+user_date = input.split('.').map!(&:to_i) # (&:to_i) does the same as { |a| a.to_i }
 days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day_number = days_per_month.take(user_date[1] - 1).sum + user_date[0]
-day_number += 1 if ((user_date[2] % 4).zero? || (user_date[2] % 400).zero?) && user_date[1] >= 3
+day_number = days_per_month.take(user_date[1] - 1).sum + user_date[0] # sum of former months and days
+day_number += 1 if ((user_date[2] % 4).zero? || (user_date[2] % 400).zero?) && user_date[1] >= 3 # leap year
 print "It appears to be day #{day_number} in #{user_date[2]}. \n"
