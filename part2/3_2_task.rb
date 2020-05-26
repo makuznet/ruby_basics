accf4d0e62f0eaccd340689313f1bcd259dd725a
Maksim Kuznetsov <makuznet@gmail.com>
2020-05-26 19:09:11 +0000

    Lesson 2

--- part2/3_2_task.rb
+++ part2/3_2_task.rb
@@ -2,16 +2,17 @@
 # frozen_string_literal: true
 
 puts 'Populating an array with Fibonacci numbers up to 100'
+
 array = [0, 1] # the first and the second Fibonacci numbers are fixed
-index = 2 # an array index
 element = 0 # Fibonacci number
+
 loop do
-  element = array[index - 1] + array[index - 2] # calculating current Fibonacci number
+  element = array[-1] + array[-2] # calculating current Fibonacci number
   if element < 100 # separating calculation from assignment
-    array[index] = element
-    index += 1
+    array.push(element) # adding a number to the array
   else
     break
   end
 end
+
 print "#{array} \n"
